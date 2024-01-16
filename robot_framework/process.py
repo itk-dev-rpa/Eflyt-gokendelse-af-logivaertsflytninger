0a3946d0d70eec2f3e02b29a9fe349bd8f902aa8
Mathias G <ghbm@aarhus.dk>
2024-01-16 08:54:56 +0100

    Process implemented

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,8 +1,27 @@
 """This module contains the main process of the robot."""
 
+import os
+
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
+
+from robot_framework import eflyt
 
 
 def process(orchestrator_connection: OrchestratorConnection) -> None:
     """Do the primary process of the robot."""
     orchestrator_connection.log_trace("Running process.")
+
+    browser = eflyt.login(orchestrator_connection)
+
+    eflyt.search_cases(browser)
+    cases = eflyt.extract_cases(browser)
+
+    for case_number in cases:
+        eflyt.handle_case(browser, case_number, orchestrator_connection)
+
+
+if __name__ == '__main__':
+    conn_string = os.getenv("OpenOrchestratorConnString")
+    crypto_key = os.getenv("OpenOrchestratorKey")
+    oc = OrchestratorConnection("Eflyt Test", conn_string, crypto_key, "")
+    process(oc)
